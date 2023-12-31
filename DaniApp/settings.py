e8ab3951a193f42a9ab7475b6407729fc67ec846
Justin Sorrells <justinwsorrells@gmail.com>
2023-12-31 11:57:47 -0700

    temp

--- DaniApp/settings.py
+++ DaniApp/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+import dj_database_url
+from os import environ
+
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,13 +82,12 @@ WSGI_APPLICATION = 'DaniApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+if "DATABASE_SECRET" in environ:
+	database_secret = environ.get("DATABASE_SECRET")
+	db_url = json.loads(database_secret)["DATABASE_URL"]
+	DATABASES = {"default": dj_database_url.parse(db_url)}
+else:
+	DATABASES = {"default": dj_database_url.parse("sqlite:///db.sqlite3")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
