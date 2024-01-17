f76d253afc6ef02d316e782d06a0aff95bdb65a9
Justin Sorrells <justinwsorrells@gmail.com>
2024-01-17 06:38:44 -0700

    trying to migrate db

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 DaniApp.wsgi
+python manage.py loaddata whole.json
+python manage.py makemigrations && python manage.py migrate && python manage.py collectstatic && gunicorn --workers 2 DaniApp.wsgi
